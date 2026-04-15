ee07144695d050c10a7f82968531414794af7592
Michael Pozhidaev <msp@luwrain.org>
2026-04-15 14:38:40 +0700

    Adding arc.mp

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -18,6 +18,14 @@ find -iname '*.png' | while read i; do
     mv "${i/.png/-crop.pdf}" "$PDF"
 done
 
+TEX=latex mpost -interaction=batchmode arch.mp end
+mptopdf arch.mps
+mv arch-mps.pdf arch.pdf
+pdfcrop arch.pdf
+rm -f arch.pdf
+mv arch-crop.pdf arch.pdf
+
+
 for i in 1 2 3; do
     pdflatex -interaction=batchmode  main.tex
 done
